8f98ccf2e931d184ef40c59fb3b2dfa506078ef7
faizanmkhan <faizanmorshid58@gmail.com>
2025-07-18 18:01:12 +0200

    branch created and changed

--- log_auto_analyze.sh
+++ log_auto_analyze.sh
@@ -13,23 +13,38 @@ LOG_FILES=$(find $LOG_DIR -name "*.log" -mtime -5)
 echo $LOG_FILES
 
 for LOG_FILE in $LOG_FILES; do
-    for ERROR in $ERROR_TYPE; do
-    echo -e "\nErrors in the $LOG_FILE log"
-    grep "${ERROR_TYPE[0]}" "$LOG_FILE"
 
-    echo -e "\nNumber of errors found in $LOG_FILE"
-    grep -c "${ERROR_TYPE[0]}" "$LOG_FILE"
+    echo -e "\n"
+    echo    "=================================================="
+    echo    "=============$LOG_FILE======================"
+    echo    "=================================================="
+    for ERROR in ${ERROR_TYPE[@]}; do
 
-    echo -e "\nNumber of fatal errors found in $LOG_FILE"
-    grep -c "${ERROR_TYPE[1]}" "$LOG_FILE"
+        echo -e "\n"
+        echo    "=================================================="
+        echo    "================$ERROR========================"
+        echo    "=================================================="
+        echo -e "\nErrors in the $LOG_FILE log"
+        grep "$ERROR" "$LOG_FILE"
 
-    echo -e "\nNumber of query in $LOG_FILE log"
-    grep -c "${ACTION_TYPE[0]}" "$LOG_FILE"
+        echo -e "\nNumber of errors found in $LOG_FILE"
+        grep -c "$ERROR" "$LOG_FILE"
 
-    echo -e "\nNumber of information found in $LOG_FILE log"
-    grep -c "${ACTION_TYPE[1]}" "$LOG_FILE"
+        echo -e "\nNumber of fatal errors found in $LOG_FILE"
+        grep -c "$ERROR" "$LOG_FILE"
+    done
+    for ACTION in ${ACTION_TYPE[@]}; do
+        echo -e "\n"
+        echo    "=================================================="
+        echo    "================$ACTION======================="
+        echo    "=================================================="
+        echo -e "\nNumber of query in $LOG_FILE log"
+        grep -c "$ACTION" "$LOG_FILE"
 
-    echo -e "\nInformation that are found in $LOG_FILE"
-    grep "${ACTION_TYPE[1]}" "$LOG_FILE"
+        echo -e "\nNumber of information found in $LOG_FILE log"
+        grep -c "$$ACTION" "$LOG_FILE"
+
+        echo -e "\nInformation that are found in $LOG_FILE"
+        grep "$$ACTION" "$LOG_FILE"
     done
 done
